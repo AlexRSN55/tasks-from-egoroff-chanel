5e57938943e1d9cb38efe7621871872f50f0e10f
Alexey Rusinov <alexrus8455@gmail.com>
2023-09-15 17:01:42 +0600

    commit for gitlab

---
+++ Netologia/Task Sceduler.py
@@ -0,0 +1,18 @@
+HELP = """
+help - print program information.
+add - add task in list (user input name of task)
+show - print all tasks in list.
+"Add comit for gitlab"
+tasks = []
+
+command = input("Enter your command: ")
+if command == "help":
+    print(HELP)
+elif command == "show":
+    print(tasks)
+elif command == "add":
+    task = input("Enter name of task: ")
+    tasks.append(task)
+    print("Task add in list")
+else:
+    print("Unknown command")
